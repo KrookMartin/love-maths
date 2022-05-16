9f0f6e12a934c41b5d7fc7641cc7f4bb7facf43d
Martin Krook <krookmartin@yahoo.com>
2022-05-16 14:35:12 +0000

    add eventlistners for docloading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName('button');
+    
+    for (let button of buttons){
+        button.addEventListener('click', function() { 
+            if(this.getAttribute('data-type')=== 'submit') {
+                alert('You clicked Submit!');
+            }
+            else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGam() {
 
 }
@@ -26,5 +43,5 @@ function displaySubtractQuestion() {
 
 }
 function displayMultiplyQuestion() {
-    
+
 }
